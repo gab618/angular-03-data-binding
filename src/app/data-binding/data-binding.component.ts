37d47c0edef9edf0400a23ef0d0dab594f77973e
Gabriel <knuckles4x@gmail.com>
2021-02-08 17:51:18 -0300

    interpolation

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css'],
+})
+export class DataBindingComponent implements OnInit {
+  url: string = 'https://github.com/gab618';
+  cursoAngular: boolean = true;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
